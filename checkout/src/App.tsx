8748c542bb5e75f5120eb677c03aebff5d9b45de
ThomasASW <125454436+ThomasASW@users.noreply.github.com>
2024-04-09 18:14:26 +0530

    Cleanup

--- checkout/src/App.tsx
+++ checkout/src/App.tsx
@@ -4,12 +4,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 const App = () => (
-  <div className="container">
-    <div>Name: checkout</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Empty CSS</div>
-  </div>
+  <></>
 )
 const rootElement = document.getElementById('app')
 if (!rootElement) throw new Error('Failed to find the root element')
