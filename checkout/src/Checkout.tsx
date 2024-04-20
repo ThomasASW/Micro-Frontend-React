a265023fd691521934f99dbf541f32299ac0cff1
ThomasASW <125454436+ThomasASW@users.noreply.github.com>
2024-04-20 19:33:03 +0530

    Add validation and show error messages

--- checkout/src/Checkout.tsx
+++ checkout/src/Checkout.tsx
@@ -20,7 +20,7 @@ export const Checkout = () => {
     const [loading, setLoading] = useState(false)
     const [disabled, setDisabled] = useState(false)
 
-    const { register, handleSubmit, reset } = useForm({
+    const { register, handleSubmit, formState: { errors } } = useForm({
         defaultValues: defaultValues
     });
 
@@ -51,13 +51,19 @@ export const Checkout = () => {
             <form>
                 <Flex style={{ width: "100%", margin: "10px", padding: "10px" }} justify='center'>
                     <Card hoverable bordered style={{ width: "60%" }} title="Checkout">
-                        <input type='text' {...register("address1", { required: true })} placeholder='Address Line 1' disabled={disabled} />
-                        <input type='text' {...register("address2", { required: true })} placeholder='Address Line 2' disabled={disabled} />
-                        <input type='text' {...register("district", { required: true })} placeholder='District' disabled={disabled} />
-                        <input type='text' {...register("state", { required: true })} placeholder='State' disabled={disabled} />
-                        <input type='text' {...register("country", { required: true })} placeholder='Country' disabled={disabled} />
-                        <input type='text' {...register("zipCode", { required: true })} placeholder='ZipCode' disabled={disabled} />
-                        <Button type='primary' loading={loading} disabled={disabled} onClick={handleSubmit(handleFormSubmit)}>{disabled ? "Order placed" : "Place order"}</Button>
+                        <input type='text' {...register("address1", { required: { value: true, message: "This field is required" }, minLength: { value: 3, message: "Min 3 chars required" } })} placeholder='Address Line 1' disabled={disabled} />
+                        {errors.address1 && <p className='errorMessage'>{errors.address1.message}</p>}
+                        <input type='text' {...register("address2", { required: { value: true, message: "This field is required" }, minLength: { value: 3, message: "Min 3 chars required" } })} placeholder='Address Line 2' disabled={disabled} />
+                        {errors.address2 && <p className='errorMessage'>{errors.address2.message}</p>}
+                        <input type='text' {...register("district", { required: { value: true, message: "This field is required" }, minLength: { value: 3, message: "Min 3 chars required" } })} placeholder='District' disabled={disabled} />
+                        {errors.district && <p className='errorMessage'>{errors.district.message}</p>}
+                        <input type='text' {...register("state", { required: { value: true, message: "This field is required" }, minLength: { value: 3, message: "Min 3 chars required" } })} placeholder='State' disabled={disabled} />
+                        {errors.state && <p className='errorMessage'>{errors.state.message}</p>}
+                        <input type='text' {...register("country", { required: { value: true, message: "This field is required" }, minLength: { value: 3, message: "Min 3 chars required" } })} placeholder='Country' disabled={disabled} />
+                        {errors.country && <p className='errorMessage'>{errors.country.message}</p>}
+                        <input type='text' {...register("zipCode", { required: { value: true, message: "This field is required" }, pattern: { value: /[0-9]{6}/, message: "Please enter a valid zip code" } })} placeholder='ZipCode' disabled={disabled} />
+                        {errors.zipCode && <p className='errorMessage'>{errors.zipCode.message}</p>}
+                        <Button style={{ marginTop: "10px" }} type='primary' loading={loading} disabled={disabled} onClick={handleSubmit(handleFormSubmit)}>{disabled ? "Order placed" : "Place order"}</Button>
                     </Card>
                 </Flex>
             </form>
