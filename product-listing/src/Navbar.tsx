0e28f5151b08ced23142a78d20c7befca2287522
ThomasASW <125454436+ThomasASW@users.noreply.github.com>
2024-05-13 12:00:34 +0530

    Use socket for dynamic cart item count

--- product-listing/src/Navbar.tsx
+++ product-listing/src/Navbar.tsx
@@ -1,38 +1,61 @@
 import { ShoppingCartOutlined, UnorderedListOutlined } from '@ant-design/icons';
-import { Menu, MenuProps } from 'antd';
-import React, { useState } from 'react'
+import { Button, Flex } from 'antd';
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import { io } from 'socket.io-client';
 
-const items: MenuProps['items'] = [
-    {
-        label: 'Products',
-        key: 'products',
-        icon: <UnorderedListOutlined />,
-    },
-    {
-        label: 'Cart',
-        key: 'cart',
-        icon: <ShoppingCartOutlined />,
-    },
-];
+const socket = io("http://localhost:5000")
 
 const Navbar = () => {
 
     const navigate = useNavigate()
 
     const [current, setCurrent] = useState('products');
+    const [cartCount, setCartCount] = useState(0);
 
-    const onClick: MenuProps['onClick'] = (e) => {
-        console.log('click ', e);
-        setCurrent(e.key);
-        if (e.key == "products") {
+    socket.on('connect', () => {
+        console.log("connected");
+    });
+    socket.on('disconnect', () => {
+        console.log("disconnected");
+    });
+    socket.on('update', (value) => {
+        console.log("update");
+        setCartCount(value.count)
+    });
+
+    const url = "http://localhost:5000/api/cart/count"
+
+    const getCartCount = async () => {
+        const res = await fetch(url);
+        const body = await res.json();
+        console.log(body);
+        setCartCount(body.count)
+    }
+
+    useEffect(() => {
+        getCartCount()
+    }, [])
+
+    const onClick = (key: string) => {
+        setCurrent(key);
+        if (key == "products") {
             navigate("/")
         } else {
             navigate("/cart")
         }
     };
 
-    return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+    return (
+        <Flex>
+            <Button onClick={() => onClick('products')} type={current == 'products' ? 'primary' : 'text'} icon={<UnorderedListOutlined />}>
+                Products
+            </Button>
+            <Button onClick={() => onClick('cart')} type={current == 'cart' ? 'primary' : 'text'} icon={<ShoppingCartOutlined />}>
+                {`Cart (${cartCount})`}
+            </Button>
+        </Flex>
+    );
 }
 
 export default Navbar
